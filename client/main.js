4abbdf16011901488139a076fdb780652a2b2a02
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2016-08-29 14:13:58 +0200

    Easier testing

--- client/main.js
+++ client/main.js
@@ -3,4 +3,21 @@ import { Template } from 'meteor/templating';
 
 import './main.html';
 
-window.Post = Post;
+Template.Posts.onCreated(function() {
+  this.subscribe('posts');
+});
+
+Template.Posts.helpers({
+  posts() {
+    return Post.find();
+  }
+});
+
+Template.Posts.events({
+  'click #add' () {
+    const p = new Post({
+      name: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10)
+    });
+    p.save();
+  }
+});
--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import Post from '/imports/Post';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.publish('posts', function() {
+  return Post.find();
 });
